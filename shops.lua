0930c25dd562192d397d5e6d85a9198a05ef03d2
Jadterfero321 <115880925+Jadterfero321@users.noreply.github.com>
2023-02-17 14:52:30 +0100

    Create shops.lua
    if its not at end of shop.lua dont forget to add "," at end

---
+++ shops.lua
@@ -0,0 +1,44 @@
+BlackMarketArms = {
+		name = 'Black Market (Arms)',
+		inventory = {
+			{ name = 'WEAPON_Carbinerifle', price = 50000, metadata = { registered = false	}, currency = 'money' },
+			{ name = 'WEAPON_Assaultrifle', price = 5000, metadata = { registered = false }, currency = 'money' },
+                        { name = 'WEAPON_Microsmg', price = 15000, metadata = { registered = false }, currency = 'money' },
+                        { name = 'WEAPON_APpistol', price = 15000, metadata = { registered = false }, currency = 'money' },
+                        { name = 'WEAPON_COMBATPDW', price = 15000, metadata = { registered = false }, currency = 'money' },
+                        { name = 'WEAPON_MINISMG', price = 30000, metadata = { registered = false }, currency = 'money' },
+                        { name = 'WEAPON_MILITARYRIFLE', price = 65000, metadata = { registered = false }, currency = 'money' },
+			{ name = 'at_suppressor_light', price = 5000, currency = 'money' },
+                        { name = 'at_suppressor_Heavy', price = 5000, currency = 'money' },
+			{ name = 'ammo-rifle', price = 10, currency = 'money' },
+			{ name = 'ammo-rifle2', price = 10, currency = 'money' },
+                        { name = 'ammo-22', price = 10, currency = 'money' },
+			{ name = 'ammo-38', price = 10, currency = 'money' },
+                        { name = 'ammo-44', price = 10, currency = 'money' },
+			{ name = 'ammo-45', price = 10, currency = 'money' },
+                        { name = 'ammo-50', price = 10, currency = 'money' },
+			{ name = 'ammo-9', price = 10, currency = 'money' },
+                        { name = 'WEAPON_Carbinerifle', price = 50000, metadata = { registered = false	}, currency = 'black_money' },
+                        { name = 'WEAPON_Microsmg', price = 15000, metadata = { registered = false }, currency = 'black_money' },
+                        { name = 'WEAPON_APpistol', price = 15000, metadata = { registered = false }, currency = 'black_money' },
+                        { name = 'WEAPON_COMBATPDW', price = 15000, metadata = { registered = false }, currency = 'black_money' },
+                        { name = 'WEAPON_MINISMG', price = 30000, metadata = { registered = false }, currency = 'black_money' },
+                        { name = 'WEAPON_MILITARYRIFLE', price = 65000, metadata = { registered = false }, currency = 'black_money' },
+			{ name = 'at_suppressor_light', price = 5000, currency = 'black_money' },
+                        { name = 'at_suppressor_Heavy', price = 5000, currency = 'black_money' },
+			{ name = 'ammo-rifle', price = 10, currency = 'black_money' },
+			{ name = 'ammo-rifle2', price = 10, currency = 'black_money' },
+                        { name = 'ammo-22', price = 10, currency = 'black_money' },
+			{ name = 'ammo-38', price = 10, currency = 'black_money' },
+                        { name = 'ammo-44', price = 10, currency = 'black_money' },
+			{ name = 'ammo-45', price = 10, currency = 'black_money' },
+                        { name = 'ammo-50', price = 10, currency = 'black_money' },
+			{ name = 'ammo-9', price = 10, currency = 'black_money' }
+
+
+		}, locations = {
+			vec3(1404.0720, -1496.4858, 59.9623)
+		}, targets = {
+                    }
+	       }
+          }
